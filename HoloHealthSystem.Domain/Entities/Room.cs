f4a1f048446a6d4a70f0b8970cbe87566ebf457f
wenderu00 <mwmcjr@gmail.com>
2022-02-17 14:00:26 -0300

    correção de encapsulamento da entidade room

--- HoloHealthSystem.Domain/Entities/Room.cs
+++ HoloHealthSystem.Domain/Entities/Room.cs
@@ -8,6 +8,7 @@ namespace HoloHealthSystem.Domain.Entities
 {
     public class Room : Entity
     {
+        private IList<Reservation> _reservations;
         public Room(string number, Clinic clinic)
         {
             Number = number;
@@ -16,12 +17,32 @@ namespace HoloHealthSystem.Domain.Entities
         public string Number { get; private set; }
         public Clinic Clinic { get; private set; }
         
+        public IReadOnlyCollection<Reservation> Reservations { get { return _reservations.ToArray(); } }
+
+
+
+
         public void UpdateNumber(string number)
         {
             if (!Clinic.HasRoomNumber(number))
             {
                 Number=number;
             }
+        }
+        public bool HasConflictReservation(Reservation reservation)
+        {
+            foreach(var x in Reservations)
+            {
+                if (x.HasConflictTime(reservation)) return true; 
+            }
+            return false;
+        }
+        public void AddReservation(Reservation reservation)
+        {
+            if (!HasConflictReservation(reservation))
+            {
+                _reservations.Add(reservation);
+            };
         }
     }
 }
