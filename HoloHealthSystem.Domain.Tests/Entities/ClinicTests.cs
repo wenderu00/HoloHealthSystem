fe2c56d33f7f082cf2bbbcc375dbf1bf617e221f
wenderu00 <mwmcjr@gmail.com>
2022-02-17 23:25:11 -0300

    Testes do consultório

--- HoloHealthSystem.Domain.Tests/Entities/ClinicTests.cs
+++ HoloHealthSystem.Domain.Tests/Entities/ClinicTests.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using HoloHealthSystem.Domain.Entities;
+using HoloHealthSystem.Domain.ValueObjects;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,78 +12,136 @@ namespace HoloHealthSystem.Domain.Tests.Entities
     [TestClass]
     public class ClinicTests
     {
+        private readonly State _state;
+        private readonly City _city;
+        private readonly Address _address;
+        private readonly Address _address2;
+        private readonly Clinic _clinic;
+        private readonly Clinic _clinic2;
+        private readonly Phone _validPhone;
+        private readonly Phone _invalidPhone;
+        private readonly Room _room;
+        private readonly Manager _manager;
+        private readonly Doctor _doctor;
+        public ClinicTests()
+        {
+            _state = new State("Pernambuco");
+            _city = new City(_state, "Recife");
+            _clinic = new Clinic("Rua da hora");
+            _clinic2 = new Clinic("Pedro leal");
+            _address = new Address("pina", "rua da hora", "202", _clinic);
+            _address2 = new Address("pina", "rua da hora", "203", _clinic2);
+            _validPhone = new Phone("81996049871");
+            _invalidPhone = new Phone("");
+            _room = new Room("203", _clinic);
+            _manager = new Manager(_clinic, new Email("facil@gmail.com"), new Name("Márcio", "Wendell"), DateTime.Now, new CPF("62318902364"));
+            _doctor = new Doctor("12345678", 0, new Email("facil@gmail.com"), new Name("Márcio", "Wendell"), DateTime.Now, new CPF("62318902364"));
+        }
+
         [TestMethod]
         [TestCategory("Entities")]
         public void Dado_um_endereco_relacionado_adiciona_endereco()
         {
-            Assert.Fail();
+            _clinic.AddAddress(_address);
+            Assert.AreEqual(_address, _clinic.Address);
         }
         [TestMethod]
         [TestCategory("Entities")]
         public void Dado_um_endereco_nao_relacionado_nao_adiciona_endereco()
         {
-            Assert.Fail();
+            _clinic.AddAddress(_address2);
+            Assert.AreEqual(null, _clinic.Address);
+            
         }
         [TestMethod]
         [TestCategory("Entities")]
         public void Dado_um_numero_invalido_nao_adiciona_o_numero()
         {
-            Assert.Fail();
+            var count = _clinic.Phones.Count;
+            _clinic.AddPhone(_invalidPhone);
+            Assert.AreEqual(count, _clinic.Phones.Count);
+            
         }
         [TestMethod]
         [TestCategory("Entities")]
         public void Dado_um_numero_repetido_nao_adiciona_o_numero()
         {
-            Assert.Fail();
+            _clinic.AddPhone(_validPhone);
+            var count = _clinic.Phones.Count;
+            _clinic.AddPhone(_validPhone);
+            Assert.AreEqual(count, _clinic.Phones.Count);
         }
         [TestMethod]
         [TestCategory("Entities")]
         public void Dado_um_numero_diferente_e_valido_adiciona_o_numero()
         {
-            Assert.Fail();
+            var count = _clinic.Phones.Count;
+            _clinic.AddPhone(_validPhone);
+            Assert.AreEqual(count+1, _clinic.Phones.Count);
         }
 
         [TestMethod]
         [TestCategory("Entities")]
         public void Dado_um_numero_remove_o_numero()
         {
-            Assert.Fail();
+            
+            _clinic.AddPhone(_validPhone);
+            var count = _clinic.Phones.Count;
+            _clinic.RemovePhone(_validPhone);
+            Assert.AreEqual(count-1, _clinic.Phones.Count);
         }
         [TestMethod]
         [TestCategory("Entities")]
         public void Dado_uma_sala_repetida_nao_adiciona()
         {
-            Assert.Fail();
+            
+            _clinic.AddRoom(_room);
+            var count = _clinic.Rooms.Count;
+            _clinic.AddRoom(_room);
+            Assert.AreEqual(count, _clinic.Rooms.Count);
         }
         [TestMethod]
         [TestCategory("Entities")]
         public void Dado_uma_sala_diferente_adiciona()
         {
-            Assert.Fail();
+            var count = _clinic.Rooms.Count;
+            _clinic.AddRoom(_room);
+            Assert.AreEqual(count+1,_clinic.Rooms.Count);
         }
         [TestMethod]
         [TestCategory("Entities")]
         public void Dado_um_gerente_repetido_nao_adiciona()
         {
-            Assert.Fail();
+            
+            _clinic.AddManager(_manager);
+            var count = _clinic.Managers.Count;
+            _clinic.AddManager(_manager);
+            Assert.AreEqual(count, _clinic.Managers.Count);
         }
         [TestMethod]
         [TestCategory("Entities")]
         public void Dado_um_gerente_diferente_adiciona()
         {
-            Assert.Fail();
+            var count = _clinic.Managers.Count;
+            _clinic.AddManager(_manager);
+            Assert.AreEqual(count+1, _clinic.Managers.Count);
         }
         [TestMethod]
         [TestCategory("Entities")]
         public void Dado_um_medico_repetido_nao_adiciona()
         {
-            Assert.Fail();
+            _clinic.AddDoctor(_doctor);
+            var count = _clinic.Doctors.Count;
+            _clinic.AddDoctor(_doctor);
+            Assert.AreEqual(count, _clinic.Doctors.Count);
         }
         [TestMethod]
         [TestCategory("Entities")]
         public void Dado_um_medico_diferente_adiciona()
         {
-            Assert.Fail();
+            var count = _clinic.Doctors.Count;
+            _clinic.AddDoctor(_doctor);
+            Assert.AreEqual(count + 1, _clinic.Doctors.Count);
         }
     }
 }
