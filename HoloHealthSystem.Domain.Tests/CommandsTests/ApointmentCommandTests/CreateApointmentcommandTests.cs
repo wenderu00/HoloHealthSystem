387dd600ecf9610adff439db83175c882d1a0db5
wenderu00 <mwmcjr@gmail.com>
2022-03-12 22:06:27 -0300

    apointement tests

--- HoloHealthSystem.Domain.Tests/CommandsTests/ApointmentCommandTests/CreateApointmentcommandTests.cs
+++ HoloHealthSystem.Domain.Tests/CommandsTests/ApointmentCommandTests/CreateApointmentcommandTests.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using HoloHealthSystem.Domain.Commands.ApointmentCommands;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,6 +11,24 @@ namespace HoloHealthSystem.Domain.Tests.CommandsTests.ApointmentCommandTests
     [TestClass]
     public class CreateApointmentcommandTests
     {
-        
+        private readonly CreateApointmentCommand _invalidCreateApointmentCommand = new CreateApointmentCommand();
+        private readonly CreateApointmentCommand _validCreateApointmentCommand = new CreateApointmentCommand("62318902364",Guid.NewGuid(),DateTime.Now);
+        public CreateApointmentcommandTests()
+        {
+            _invalidCreateApointmentCommand.Validate();
+            _validCreateApointmentCommand.Validate();
+        }
+        [TestMethod]
+        [TestCategory("Commands")]
+        public void Should_is_valid_return_false_when_invalid_command_is_given()
+        {
+            Assert.IsFalse(_invalidCreateApointmentCommand.IsValid);
+        }
+        [TestMethod]
+        [TestCategory("Commands")]
+        public void Should_is_valid_return_true_when_valid_command_is_given()
+        {
+            Assert.IsTrue(_validCreateApointmentCommand.IsValid);
+        }
     }
 }
